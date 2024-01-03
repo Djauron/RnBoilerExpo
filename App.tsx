cfe0e1b6b3735267b3fc79965ac28cd5dcec7c76
Sinan <sinan.daglar@shadow.tech>
2024-01-03 14:47:36 +0100

    second commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -7,9 +7,6 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-      />
     </View>
   );
 }
